9823aa7db1b496fb82b745b126a1fb681211ee01
amirabasbakhshipoor <159625780+amirabasbakhshipoor@users.noreply.github.com>
2024-02-10 18:36:30 +0330

    html

--- نمرین/kafsh.js
+++ نمرین/kafsh.js
@@ -5,7 +5,7 @@ function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
-    
+    slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
